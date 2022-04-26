ca418d70fbd91176dee5170a5e0529804f8920e9
jonsak06 <jona-hhh@hotmail.com>
2022-04-25 22:37:54 -0300

    Add project files.

---
+++ MiddlewareArchivos/Window.cs
@@ -0,0 +1,16 @@
+namespace MiddlewareArchivos
+{
+    public partial class Window : Form
+    {
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
