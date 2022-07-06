f438e80d34a2395bb3464fb8e5a77f91c71b443c
jonsak06 <jona-hhh@hotmail.com>
2022-07-06 01:20:34 -0300

    Agregada aplicación de consola para creación de carpetas

---
+++ CrearCarpetasMiddleware/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Configuration;
+
+string Path = $"{ConfigurationManager.AppSettings["Path"]}";
+
+string PathCarpetaIn = $"{Path}{ConfigurationManager.AppSettings["CarpetaIN"]}\\";
+string PathCarpetaOut = $"{Path}{ConfigurationManager.AppSettings["CarpetaOUT"]}\\";
+string PathCarpetaConfig = $"{Path}{ConfigurationManager.AppSettings["CarpetaCONFIG"]}\\";
+string PathCarpetaCtrl = $"{Path}{ConfigurationManager.AppSettings["CarpetaCTRL"]}\\";
+
+string PathCarpetaInPendiente = $"{PathCarpetaIn}{ConfigurationManager.AppSettings["CarpetaPendienteIN"]}\\";
+string PathCarpetaInEnProceso = $"{PathCarpetaIn}{ConfigurationManager.AppSettings["CarpetaEnProcesoIN"]}\\";
+string PathCarpetaInProcesado = $"{PathCarpetaIn}{ConfigurationManager.AppSettings["CarpetaProcesadoIN"]}\\";
+string PathCarpetaInNoProcesado = $"{PathCarpetaIn}{ConfigurationManager.AppSettings["CarpetaNoProcesadoIN"]}\\";
+string PathCarpetaInLog = $"{PathCarpetaIn}{ConfigurationManager.AppSettings["CarpetaLogIN"]}\\";
+
+string PathCarpetaOutEnProceso = $"{PathCarpetaOut}{ConfigurationManager.AppSettings["CarpetaEnProcesoOUT"]}\\";
+string PathCarpetaOutPendiente = $"{PathCarpetaOut}{ConfigurationManager.AppSettings["CarpetaPendienteOUT"]}\\";
+string PathCarpetaOutBackup = $"{PathCarpetaOut}{ConfigurationManager.AppSettings["CarpetaBackupOUT"]}\\";
+string PathCarpetaOutLog = $"{PathCarpetaOut}{ConfigurationManager.AppSettings["CarpetaLogOUT"]}\\";
+
+List<string> carpetasPrincipales = new List<string> { PathCarpetaIn, PathCarpetaOut, PathCarpetaConfig, PathCarpetaCtrl };
+List<string> subCarpetas = new List<string> { PathCarpetaInPendiente, PathCarpetaInEnProceso, PathCarpetaInProcesado, PathCarpetaInNoProcesado, PathCarpetaInLog,
+                PathCarpetaOutEnProceso, PathCarpetaOutPendiente, PathCarpetaOutBackup, PathCarpetaOutLog };
+
+
+foreach (string carpeta in carpetasPrincipales)
+{
+    if (!Directory.Exists(carpeta))
+    {
+        Directory.CreateDirectory(carpeta);
+    }
+}
+
+foreach (string subCarpeta in subCarpetas)
+{
+    if (!Directory.Exists(subCarpeta))
+    {
+        Directory.CreateDirectory(subCarpeta);
+    }
+}
+
+Console.WriteLine("Proceso finalizado");
